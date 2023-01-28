fdb20cb89abbfb045947967ad9a28ff45bb0420c
Jphn <joaopedroholandaneves@gmail.com>
2023-01-28 13:56:46 -0300

    fix(Home): overflow hidden  being applied in home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,16 @@
+import { globalCss } from '@stitches/react';
 import CreditsSection from '../layouts/CreditsSection';
 import MainSection from '../layouts/MainSection';
 
+const globalStyles = globalCss({
+	body: {
+		overflow: 'auto',
+	},
+});
+
 export default function () {
+	globalStyles();
+
 	return (
 		<>
 			<MainSection />
